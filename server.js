1453949169eb3c8c064ebcda9c24165441047e23
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-27 09:38:54 +0300

    commit

--- server.js
+++ server.js
@@ -11,11 +11,6 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const path = require("path");
-app.set('view engine', 'ejs');
-app.use(expressLayouts);
-
-
-
 
 // Define the directory where your static files are located
 const publicDirectoryPath = path.join(__dirname, 'public');
