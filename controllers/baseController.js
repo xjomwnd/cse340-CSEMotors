853235832000b9c630b6484006bf38da31c363c9
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-05-08 15:33:51 +0300

    commit

---
+++ controllers/baseController.js
@@ -0,0 +1,12 @@
+# Query: 
+# ContextLines: 1
+
+const utilities = require("../utilities/")
+const baseController = {}
+
+baseController.buildHome = async function(req, res){
+  const nav = await utilities.getNav()
+  res.render("index", {title: "Home", nav})
+}
+
+module.exports = baseController
