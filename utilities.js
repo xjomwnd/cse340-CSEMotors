5c36ec984c8f734092e70ba2436f65cb36f58b16
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-05-09 06:38:02 +0300

    commit

--- utilities.js
+++ utilities.js
@@ -1,14 +1,1 @@
-// utilities.js
-function handleErrors(fn) {
-  return (req, res, next) => {
-    try {
-      fn(req, res, next);
-    } catch (err) {
-      console.error(err); // Log the error for now
-      // You can also send an error response to the client here
-      res.status(500).send("Internal Server Error");
-    }
-  };
-}
 
-module.exports = handleErrors;
