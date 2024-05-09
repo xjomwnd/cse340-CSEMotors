5851a5577bb836d2358335f4211f39d46deba4f7
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-05-09 03:22:37 +0300

    commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
  *****************************/
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const env = require("dotenv").config();
+const .env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
 const path = require("path");
